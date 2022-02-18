2724c6861b901b0700e6298b4f12ffa1499f6b2e
Luca Biavati <lucabiavati@Lucas-MacBook-Pro.local>
2022-02-18 17:59:24 -0500

    re-analyze data from paper

---
+++ szabo_scRNAseqTcell.R
@@ -0,0 +1,27 @@
+rm(list = ls())
+
+library(Seurat)
+library(tidyverse)
+library(SCPA)
+
+
+# Set PrimaryDirectory where this script is located
+dirname(rstudioapi::getActiveDocumentContext()$path)  
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+getwd()
+PrimaryDirectory <- getwd()
+PrimaryDirectory
+
+# create working directory
+workingDir <- "WorkingDirectory_D0"
+dirPath <- file.path(PrimaryDirectory, workingDir)
+dir.create(dirPath)
+setwd(dirPath)
+
+saveDir <- file.path(dirPath, "results")
+savePath <- saveDir
+dir.create(saveDir)
+
+filesPath <- file.path(dirPath, "files")
+dir.create(filesPath)
+
